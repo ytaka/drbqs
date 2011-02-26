149a3dd6281e7d51e6fa076d6902907b88dd2526
Takayuki YAMAGUCHI <d@ytak.info>
2011-02-26 16:05:04 +0900

    Manage calculating tasks until server deletes at accepting results.

--- lib/drbqs/queue.rb
+++ lib/drbqs/queue.rb
@@ -28,7 +28,7 @@ module DRbQS
       @result = result
       @task_id = 0
       @cache = {}
-      @calculating = {}
+      @calculating = Hash.new { |hash, key| hash[key] = Array.new }
       @logger = logger
     end
 
@@ -49,7 +49,7 @@ module DRbQS
       begin
         loop do
           sym, task_id, node_id = @result.take([:accept, Fixnum, Fixnum], 0)
-          @calculating[node_id] = task_id
+          @calculating[node_id] << task_id
           @logger.info("Accept: task #{task_id} by node #{node_id}.") if @logger
         end
       rescue
@@ -58,9 +58,9 @@ module DRbQS
 
     def requeue_for_deleted_node_id(deleted)
       deleted.each do |node_id|
-        if task_id = @calculating[node_id]
-          queue_task(task_id)
-          @calculating.delete(task_id)
+        if task_id_ary = @calculating[node_id]
+          task_id_ary.each { |task_id| queue_task(task_id) }
+          @calculating.delete(node_id)
         end
       end
     end
@@ -72,9 +72,13 @@ module DRbQS
           get_accept_signal
           sym, task_id, result = @result.take([:result, Fixnum, nil], 0)
           count += 1
-          node_id = @calculating.key(task_id)
+          if ary = @calculating.find { |k, v| v.include?(task_id) }
+            node_id = ary[0]
+            @calculating[node_id].delete(task_id)
+          else
+            node_id = nil
+          end
           @logger.info("Get: result of #{task_id} from node #{node_id}.") if @logger
-          @calculating.delete(node_id)
           task = @cache.delete(task_id)
           if hook = task.hook
             hook.call(self, result)
@@ -85,21 +89,21 @@ module DRbQS
       count
     end
 
+    def calculating_task_number
+      @calculating.inject(0) { |s, key_val| s + key_val[1].size }
+    end
+
     # If queue is empty, return true. Otherwise, false.
     # Even if there are calculating tasks,
     # the method can return true.
     def empty?
-      @cache.size - @calculating.size == 0
+      @cache.size - calculating_task_number == 0
     end
 
     # If there are no tasks in queue and calculating,
     # return true. Otherwise, false.
     def finished?
       @cache.size == 0
-    end
-
-    def calculating_task_number
-      @calculating.size
     end
   end
 
