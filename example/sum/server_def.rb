77f62c476f440f26dd9268b54337067afd321846
Takayuki YAMAGUCHI <d@ytak.info>
2011-03-05 10:41:26 +0900

    Implement server specific options.

--- example/sum/server_def.rb
+++ example/sum/server_def.rb
@@ -1,7 +1,22 @@
+# 
+# Usage:
+#  drbqs-server server_def.rb -- 30 50
+#  drbqs-server server_def.rb -- 100 500 --step 100
+# 
+
 require_relative 'sum.rb'
 
-DRbQS.define_server do |server|
-  10.step(100, 10) do |i|
+DRbQS.option_parser do |opt, hash|
+  opt.on('--step NUM', Integer) do |v|
+    hash[:step] = v
+  end
+end
+
+DRbQS.define_server do |server, argv, opts|
+  start_num = (argv[0] || 10).to_i
+  end_num = (argv[1] || 100).to_i
+  step_num = opts[:step] || 10
+  start_num.step(end_num, step_num) do |i|
     task = DRbQS::Task.new(Sum.new(i - 10, i), :exec)
     server.queue.add(task)
   end
--- lib/drbqs/server_define.rb
+++ lib/drbqs/server_define.rb
@@ -1,22 +1,71 @@
 module DRbQS
 
-  @@server_create = nil
+  class ServerDefinition
+    HELP_MESSAGE =<<HELP
+[Server specific options]
+  These options are separated by '--' from command options.
+
+HELP
+
+    def initialize
+      @server_create = nil
+      @option_parse = nil
+      @opts = {}
+      @argv = nil
+    end
+
+    def define_server(&block)
+      if @server_create
+        raise ArgumentError, "The server has already defined."
+      end
+      @server_create = block
+    end
+
+    def option_parser(&block)
+      if @option_parse
+        raise ArgumentError, "The options parser has already defined."
+      end
+      @option_parse = block
+    end
+
+    def parse_option(opt_argv)
+      if @option_parse
+        OptionParser.new(HELP_MESSAGE) do |opt|
+          @option_parse.call(opt, @opts)
+          opt.parse!(opt_argv)
+        end
+      end
+      @argv = opt_argv  
+    end
+
+    def start_server(options)
+      unless @server_create
+        raise "Can not get server definition."
+      end
+      server = DRbQS::Server.new(options)
+      @server_create.call(server, @argv, @opts)
+      # @server_create.call(server)
+      server.set_signal_trap
+      server.start
+      server.wait
+    end
+  end
+
+  @@server_def = ServerDefinition.new
 
   def self.define_server(&block)
-    if @@server_create
-      raise ArgumentError, "The server has already defined."
-    end
-    @@server_create = block
+    @@server_def.define_server(&block)
+  end
+
+  def self.option_parser(&block)
+    @@server_def.option_parser(&block)
+  end
+
+  def self.parse_option(opt_argv)
+    @@server_def.parse_option(opt_argv)
   end
 
   def self.start_server(options)
-    unless @@server_create
-      raise "Can not get server definition."
-    end
-    server = DRbQS::Server.new(options)
-    @@server_create.call(server)
-    server.set_signal_trap
-    server.start
-    server.wait
+    @@server_def.start_server(options)
   end
 end
