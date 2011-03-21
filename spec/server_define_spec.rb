b6ad18a40128f19ea637b0c0cb554f3e1f27fc06
Takayuki YAMAGUCHI <d@ytak.info>
2011-03-21 09:51:56 +0900

    Fix server_define_spec.rb for some modifications.

--- spec/server_define_spec.rb
+++ spec/server_define_spec.rb
@@ -7,28 +7,39 @@ describe DRbQS::ServerDefinition do
     end
 
     it "should define server" do
-      @server_definition.should_receive(:define_server)
-      DRbQS.define_server do |server, argv, opts|
-        server.add_hook(:finish) do |serv|
-          serv.exit
+      lambda do
+        DRbQS.define_server do |server, argv, opts|
+          server.add_hook(:finish) do |serv|
+            serv.exit
+          end
         end
-      end
+      end.should change { @server_definition.instance_variable_get(:@default_server_opts) }.from(nil).to({})
     end
 
     it "should set parser of options" do
-      @server_definition.should_receive(:option_parser)
-      DRbQS.option_parser do |opt, hash|
-        opt.on('--test') do |v|
-          hash[:test] = true
+      lambda do
+        DRbQS.option_parser do |opt, hash|
+          opt.on('--test') do |v|
+            hash[:test] = true
+          end
         end
-      end
+      end.should change { @server_definition.instance_variable_get(:@option_parse) }.from(nil)
     end
 
     it "should parse options" do
-      @server_definition.should_receive(:parse_option)
-      DRbQS.parse_option(['--test'])
+      lambda do
+        DRbQS.parse_option(['--test'])
+      end.should change { @server_definition.instance_variable_get(:@argv) }.from(nil)
     end
 
+    it "should start server" do
+      DRbQS::Server.should_receive(:new)
+      begin
+        # After DRbQS::Server.new returns nil, raise error
+        DRbQS.start_server(:uri => 'druby://localhost:13500')
+      rescue
+      end
+    end
   end
 
 end
