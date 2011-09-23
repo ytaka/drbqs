d492ed6996ee76c8400de960f53a52faaeefb6e0
Takayuki YAMAGUCHI <d@ytak.info>
2011-09-23 18:38:21 +0900

    Add DRbQS::Worker class to manage processes and tasks.

--- lib/drbqs/worker/worker.rb
+++ lib/drbqs/worker/worker.rb
@@ -1,170 +1,101 @@
-require 'drbqs/worker/serialize'
-require 'drbqs/worker/forked_process'
-require 'drbqs/utility/temporary.rb'
+require 'drbqs/task/task'
+require 'drbqs/worker/worker_process_set'
 
 module DRbQS
   class Worker
-    READ_BYTE_SIZE = 1024
-
     attr_reader :process
 
-    def initialize
-      @process = {}
-      @result = Queue.new
-    end
-
-    def process_create(key)
-      io_r, io_w = IO.pipe('BINARY')
-      io_r2, io_w2 = IO.pipe('BINARY')
-      pid = fork do
-        io_w.close
-        io_r2.close
-        worker = ForkedProcess.new(io_r, io_w2)
-        worker.start
+    def initialize(opts = {})
+      @process = DRbQS::Worker::ProcessSet.new(opts[:class])
+      if opts[:key]
+        opts[:key].each do |key|
+          @process.create_process(key)
+        end
       end
-      @process[key] = {
-        :pid => pid, :out => io_w, :in => io_r2,
-        :unpacker => DRbQS::Worker::Serialize::Unpacker.new,
-        :task => []
-      }
+      @state = Hash.new { |h, k| h[k] = Hash.new }
+      @task_pool = {}
+      @task_group = Hash.new { |h, k| h[k] = Array.new }
+      @task_num = 0
     end
-    private :process_create
 
-    def get_process(key)
-      if @process[key]
-        @process[key]
-      else
-        process_create(key)
-      end
-    end
-    private :get_process
-
-    def create_process(*keys)
+    def sleep(*keys)
       keys.each do |key|
-        get_process(key)
+        @state[key][:sleep] = true
       end
     end
 
-    def exist?(key)
-      @process[key]
-    end
-
-    def output_to_io(io, obj)
-      io.print Serialize.dump(obj)
-      io.flush
-    end
-    private :output_to_io
-
-    def send_object(key, obj)
-      if h = get_process(key)
-        output_to_io(h[:out], obj)
-        h
-      else
-        nil
-      end
-    end
-    private :send_object
-
-    # @param [Array] dumped_task_ary is [id, obj, method_name, args].
-    def send_task(key, dumped_task_ary)
-      if h = send_object(key, dumped_task_ary)
-        h[:task] << dumped_task_ary[0]
-      else
-        raise "Process #{key.inspect} does not exist."
+    def wakeup(*keys)
+      keys.each do |key|
+        @state[key][:sleep] = false
       end
     end
 
-    def send_task_to_waiting_process(dumped_task_ary)
-      key_not_working = nil
-      @process.each do |key, h|
-        if h[:task].empty?
-          key_not_working = key
+    def group(grp, *keys)
+      keys.each do |key|
+        (@state[key][:group] ||= []) << grp
+      end
+    end
+
+    def on_result(&block)
+      @process.on_result do |proc_key, ary|
+        task_id, result = ary
+        if task_data = @task_pool.delete(task_id)
+          task = task_data[:task]
+          @task_group[task.group].delete(task_id)
+          task.exec_hook(self, result)
         end
-      end
-      if key_not_working
-        send_task(key_not_working, dumped_task_ary)
-        key_not_working
-      else
-        nil
+        block.call(proc_key, ary)
       end
     end
 
-    def prepare_to_exit(key = nil)
-      if key
-        if h = send_object(key, :prepare_to_exit)
-          h[:exit] = true
-        end
-      else
-        @process.each do |key, h|
-          prepare_to_exit(key)
-        end
-      end
+    def on_error(&block)
+      @process.on_error(&block)
     end
 
-    def delete_process(key)
-      if h = get_process(key)
-        Process.detach(h[:pid])
-        output_to_io(h[:out], :exit)
-      else
-        nil
-      end
+    def add_task(task)
+      task_id = (@task_num += 1)
+      @task_pool[task_id] = { :task => task }
+      @task_group[task.group] << task_id
     end
-    private :delete_process
 
-    def respond_signal(&block)
-      if block_given?
-        num = 0
-        to_be_deleted = []
-        @process.each do |key, h|
-          if !h[:task].empty? || h[:exit]
-            begin
-              data = h[:in].read_nonblock(READ_BYTE_SIZE)
-              h[:unpacker].feed_each(data) do |ary|
-                num += 1
-                response_type, response = ary
-                case response_type
-                when :result
-                  h[:task].delete(response[:id])
-                  yield(key, response_type, response)
-                when :node_error
-                  yield(key, response_type, response)
-                when :finish_preparing_to_exit
-                  delete_process(key)
-                  to_be_deleted << key
+    def step
+      if waiting = @process.waiting_process
+        waiting.each do |proc_key|
+          if @state[proc_key][:sleep]
+            next
+          end
+          catch(:add) do
+            grps = (@state[proc_key][:group] || []) + [DRbQS::Task::DEFAULT_GROUP]
+            grps.each do |gr|
+              @task_group[gr].each do |task_id|
+                task_data = @task_pool[task_id]
+                if !task_data[:calculate]
+                  dumped = [task_id] + task_data[:task].simple_drb_args
+                  @process.send_task(proc_key, dumped)
+                  @task_pool[task_id][:calculate] = true
+                  throw :add
                 end
               end
-            rescue IO::WaitReadable
             end
           end
         end
-        to_be_deleted.each do |key|
-          @process.delete(key)
-        end
-        to_be_deleted.clear
-        num > 0
-      else
-        to_enum(:respond_signal)
       end
+      @process.respond_signal
     end
 
-    def kill_all_processes
-      @process.each do |key, h|
-        Process.detach(h[:pid])
-        Process.kill("KILL", h[:pid])
-      end
-      @process.clear
-    end
-
-    WAITALL_INTERVAL_TIME = 0.1
-
-    def waitall
-      unless @process.empty?
-        return nil
-      end
-      until Process.waitall == []
-        sleep(WAITALL_INTERVAL_TIME)
-      end
-      true
-    end
+      # def send_task_to_waiting_process(dumped_task_ary)
+      #   key_not_working = nil
+      #   @process.each do |key, h|
+      #     if h[:task].empty?
+      #       key_not_working = key
+      #     end
+      #   end
+      #   if key_not_working
+      #     send_task(key_not_working, dumped_task_ary)
+      #     key_not_working
+      #   else
+      #     nil
+      #   end
+      # end
   end
 end
--- spec/worker/worker_spec.rb
+++ spec/worker/worker_spec.rb
@@ -2,13 +2,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 require 'drbqs/worker/worker'
-require 'drbqs/task/task'
-require 'sys/proctable'
 
 describe DRbQS::Worker do
+  class SavePID
+    def output(path)
+      pid = Process.pid
+      open(path, 'a+') do |f|
+        f.puts pid
+      end
+      pid
+    end
+  end
+
   before(:all) do
     @worker = DRbQS::Worker.new
-    @task_args = DRbQS::Task.new("hello world", :to_s).simple_drb_args
+    @error = []
+    @result = []
+    @worker.on_error do |proc_key, error|
+      @error << [proc_key, error]
+    end
+    @worker.on_result do |proc_key, result|
+      @result << [proc_key, result]
+    end
+    @worker.process.create_process(:proc1, :proc2, :proc3)
+    @worker.group(:gr1, :proc1)
+    @worker.group(:gr3, :proc3)
     @n = 0
   end
 
@@ -16,73 +34,100 @@ describe DRbQS::Worker do
     @worker
   end
 
-  def send_task(key)
-    subject.send_task(key, [(@n += 1)] + @task_args)
+  def file_path(key)
+    File.join(File.dirname(__FILE__), "test_worker", "#{key}.txt")
   end
 
-  it "should create new process." do
-    key = :proc1
-    send_task(key)
-    pid = subject.process[key][:pid]
-    Sys::ProcTable.ps(pid).should be_true
-    get_result_key = nil
+  def send_task(key, group = nil)
+    path = file_path(key)
+    FileUtils.mkdir(File.dirname(path)) unless File.exist?(File.dirname(path))
+    task = DRbQS::Task.new(SavePID.new, :output, args: [path], group: group) do |wk, res|
+      puts "Hook: receive #{res}"
+    end
+    subject.add_task(task)
+  end
+
+  it "should execute a task." do
+    task_key = :task
+    send_task(task_key)
     loop do
-      subject.respond_signal do |k, res|
-        get_result_key = k
-      end
-      if get_result_key
+      subject.step
+      unless @result.empty?
         break
       end
       sleep(0.1)
     end
-    get_result_key.should == key
+    File.read(file_path(task_key)).lines.to_a[-1].strip.to_i.should == @result[0][1][1]
   end
 
-  it "should make process exit." do
-    key = :proc2
-    send_task(key)
-    pid = subject.process[key][:pid]
-    subject.prepare_to_exit(key)
+  it "should execute a task on process of particular group." do
+    pid = subject.process.process[:proc3][:pid]
+    task_keys = [:task2, :task3]
+    task_keys.each do |key|
+      send_task(key, :gr3)
+    end
     loop do
-      subject.respond_signal do |k, res|
-      end
-      unless subject.process[key]
-        sleep(0.1)
+      subject.step
+      if @result.size == 2
         break
       end
       sleep(0.1)
     end
-    Sys::ProcTable.ps(pid).should be_nil
+    task_keys.each do |key|
+      File.read(file_path(key)).lines.to_a[-1].strip.to_i.should == pid
+    end
   end
 
-  it "should make all process exit." do
-    [:proc3, :proc4].each do |key|
+  it "should sleep nodes." do
+    subject.sleep(:proc1, :proc2)
+    pid = subject.process.process[:proc3][:pid]
+    task_keys = [:task4, :task5]
+    task_keys.each do |key|
       send_task(key)
     end
-    subject.prepare_to_exit
     loop do
-      subject.respond_signal do |k, res|
-      end
-      if subject.process.empty?
-        sleep(0.1)
+      subject.step
+      if @result.size == 2
         break
       end
       sleep(0.1)
     end
-    Process.waitall.should == []
+    task_keys.each do |key|
+      File.read(file_path(key)).lines.to_a[-1].strip.to_i.should == pid
+    end
+    subject.wakeup(:proc1, :proc2)
   end
 
-  it "should kill all processes compellingly." do
-    [:proc5, :proc6].each do |key|
+  it "should wakeup a node." do
+    subject.sleep(:proc1, :proc2, :proc3)
+    subject.wakeup(:proc2)
+    pid = subject.process.process[:proc2][:pid]
+    task_keys = [:task6, :task7]
+    task_keys.each do |key|
       send_task(key)
     end
-    subject.kill_all_processes
-    subject.waitall
-    Process.waitall.should == []
+    loop do
+      subject.step
+      if @result.size == 2
+        break
+      end
+      sleep(0.1)
+    end
+    task_keys.each do |key|
+      File.read(file_path(key)).lines.to_a[-1].strip.to_i.should == pid
+    end
+    subject.wakeup(:proc1, :proc3)
+  end
+
+  after(:each) do
+    @result.clear
+    @error.clear
   end
 
   after(:all) do
-    subject.kill_all_processes
-    subject.waitall
+    subject.process.kill_all_processes
+    subject.process.waitall
+    path = File.join(File.dirname(__FILE__), "test_worker")
+    FileUtils.rm_r(path) if File.exist?(path)
   end
 end
