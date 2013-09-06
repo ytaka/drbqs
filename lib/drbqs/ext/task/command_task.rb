e889bdaae81cb939eadeedba893b61f49e151478
Takayuki YAMAGUCHI <d@ytak.info>
2013-09-06 21:49:05 +0900

    Change arguments of CommandTask

--- lib/drbqs/ext/task/command_task.rb
+++ lib/drbqs/ext/task/command_task.rb
@@ -4,26 +4,23 @@ module DRbQS
 
     # Execute a command and transfer files if needed.
     class CommandExecute
-
-      # :transfer    String or Array
-      # :compress    true or false
-      def initialize(cmd, opts = {})
-        @cmd = cmd
-        unless (Array === @cmd || String === @cmd)
-          raise ArgumentError, "Invalid command: #{@cmd.inspect}"
+      def initialize(cmds, opts = {})
+        opts.assert_valid_keys(:transfer, :compress)
+        @cmds = cmds
+        if String === @cmds
+          @cmds = [@cmds]
+        elsif !(Array === @cmds)
+          raise ArgumentError, "Invalid command: #{@cmds.inspect}"
         end
         @transfer = opts[:transfer]
         @compress = opts[:compress]
       end
 
       def exec
-        case @cmd
-        when Array
-          @cmd.each { |c| system(c) }
-        when String
-          system(@cmd)
+        exit_status_ary = @cmds.map do |cmd|
+          system(cmd)
+          $?.exitstatus
         end
-        exit_status = $?.exitstatus
         if @transfer
           if @transfer.respond_to?(:each)
             @transfer.each { |path| DRbQS::Transfer.enqueue(path, :compress => @compress) }
@@ -31,13 +28,19 @@ module DRbQS
             DRbQS::Transfer.enqueue(@transfer, :compress => @compress)
           end
         end
-        exit_status
+        exit_status_ary
       end
     end
 
-    # &hook takes a server instance and exit number of command.
-    def initialize(cmd, opts = {}, &hook)
-      super(DRbQS::CommandTask::CommandExecute.new(cmd, opts), :exec, &hook)
+    # @override initialize(*cmds, opts = {}, &hook)
+    #  @param [Array] cmds An array of commands which are string or array, that is, arguments of the method 'system'.
+    #  @param [Hash] opts Hash of options
+    #  @param [Proc] hook Block same as that of DRbQS::Task object, which takes a server instance and an array of exit statuses.
+    #  @option opts [String,Array] :transfer Paths to be transfered after finish of commands
+    #  @option opts [boolean] :compress Compress files before transfering or not
+    def initialize(*args, &hook)
+      opts = args.extract_options!
+      super(DRbQS::CommandTask::CommandExecute.new(args, opts), :exec, &hook)
     end
   end
 end
